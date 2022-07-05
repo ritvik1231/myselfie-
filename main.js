7da3a898716d4a4a90f8183ac298b5f316fc74ef
ritvik1231 <92087795+ritvik1231@users.noreply.github.com>
2022-07-05 17:37:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,24 @@ recognition.onresult=function(event){
     console.log(content)
 
     document.getElementById("textbox").innerHTML=content;
+
+    speak()//calling a function
 }
+
+function speak(){ //creating a function
+    var synth=window.speechSynthesis;
+    var speak_data=document.getElementById("textbox").value;
+    var utterThis=new SpeechSynthesisUtterance(speak_data)
+synth.speak(utterThis)
+
+Webcam.attach(camera) //to open the camera
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:"png",
+    png_quality:90
+});
+
+var camera=document.getElementById("camera");
