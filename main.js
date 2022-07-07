b8f4dede4e1a554118fe753c1b2ba3992e67c217
ritvik1231 <92087795+ritvik1231@users.noreply.github.com>
2022-07-07 17:50:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,18 +11,33 @@ recognition.onresult=function(event){
     var content=event.results[0][0].transcript;
     console.log(content)
 
+    if(content=="take my selfie"){
+        console.log("take my selfie")
+        speak()//calling a function
+    }
+
     document.getElementById("textbox").innerHTML=content;
 
-    speak()//calling a function
+ 
 }
 
 function speak(){ //creating a function
     var synth=window.speechSynthesis;
-    var speak_data=document.getElementById("textbox").value;
+    var speak_data="Taking your selfie in 5 seconds"
     var utterThis=new SpeechSynthesisUtterance(speak_data)
 synth.speak(utterThis)
 
+
+
 Webcam.attach(camera) //to open the camera
+
+ setTimeout(function()
+{
+    take_snapshot()
+    save()
+
+},5000)
+
 }
 
 Webcam.set({
@@ -33,3 +48,17 @@ Webcam.set({
 });
 
 var camera=document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>'
+    })
+}
+
+function save(){
+    var link=document.getElementById("link")
+    var image=document.getElementById("selfie_image").src
+    link.href=image
+    link.click()
+}
